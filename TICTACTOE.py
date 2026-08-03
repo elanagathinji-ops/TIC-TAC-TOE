545c83c657e8ae7889fe976ae63e2531055bddf1
Elana Gathinji <trainee.elana.gathinji@sigmalabs.co.uk>
2026-08-03 07:35:56 +0100

    Creating a function to check game status

--- TICTACTOE.py
+++ TICTACTOE.py
@@ -1,9 +1,11 @@
 # Welcome to TIC TAC TOE
 
 import random
+# Assign players and print blank grid
 print("**** Welcome to TIC TAC TOE!**** \n")
 p1 = 'tim'  # input("Enter a name for Player 1: ").title()
 p2 = 'sarah'  # input("Enter a name for Player 2: ").title()
+grid = [[' ' for k in range(3)] for j in range(3)]
 
 # Randomly allocate who goes first
 while True:
@@ -17,16 +19,11 @@ while True:
     else:
         continue
 
-grid = [[' ' for k in range(3)] for j in range(3)]
-
 
 def print_grid(grid):
     for row in grid:
         print(" | ".join(row))
         print("-" * 9)
-
-
-print_grid(grid)
 
 # Choose a coordinate on a grid
 
@@ -49,26 +46,55 @@ def choose_coord():
             print("Invalid input - please enter either 1, 2 or 3")
     return [row-1,  column-1]
 
-
 # Draw 'X' or 'O' in the grid
 # Check if coord is already full
+
+
 def draw_symbol():
-    x = choose_coord()
-    row = x[0]
-    column = x[1]
-    if grid[row][column] != " ":
-        print("Oops! That spot is taken, try again:")
-        choose_coord()
-    else:
-        if choice == 1:
-            grid[row][column] == 'X'
+    while True:
+        row, column = choose_coord()
+        if grid[row][column] != " ":
+            print("Oops! That spot is taken, try again.")
+            continue
         else:
-            grid[row][column] == 'O'
-    print_grid(grid)
+            if choice == 1:
+                grid[row][column] = 'X'
+            else:
+                grid[row][column] = 'O'
+            break
+    return grid
 
 
-choice *= -1
-if choice == 1:
-    print(f"Now its {p1}'s turn! \n")
-else:
-    print(f"Now its {p2}'s turn! \n")
+# check for
+
+# Switch turns
+
+def switch_turns():
+    choice *= -1
+    if choice == 1:
+        print(f"Now its {p1}'s turn! \n")
+    else:
+        print(f"Now its {p2}'s turn! \n")
+
+
+# Need to check if play has won after 3 turns
+def game_status():
+    for row in grid:
+        if row == ['X', 'X', 'X'] or row[0] == row[1] and row[1] == row[2] and row[0] == "X":
+            print(f"Congratulations, {p1}! You win!")
+        elif row == ['O', 'O', 'O'] or row[0] == row[1] and row[1] == row[2] and row[0] == "O":
+            print(f"Congratulations, {p2}! You win!")
+        else:
+            continue
+
+
+# Game must end if a player wins or board is full (=draw)
+
+print_grid(grid)
+# Game play
+
+
+draw_symbol()
+print()
+print_grid(grid)
+game_status()
